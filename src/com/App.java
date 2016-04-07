a2ca5d7219d4e1d1fcc22d06f722db1e1e6c0667
hsoft3 <jiangbai333@163.com>
2016-04-07 15:35:41 +0800

    修改一些缩进

--- src/com/App.java
+++ src/com/App.java
@@ -13,30 +13,30 @@ import com.sys.libs.Router;
 
 @SuppressWarnings("serial")
 public final class App extends HttpServlet {
-	
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    	System.out.println("doGet");
-    }
-
+	        throws ServletException, IOException {
+	
+		System.out.println("doGet");
+	}
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    	System.out.println("doPost");
-    }
-    
+	        throws ServletException, IOException {
+	
+		System.out.println("doPost");
+	}
+	
 	/**
 	 * 重载 service 方法, 为应用提供一个统一的入口
 	 */
-    public void service(HttpServletRequest request, HttpServletResponse response)
+	public void service(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-    	
-    	/** 设置字符集编码 */
-    	response.setContentType("text/html; charset=UTF-8");
-    	
-    	/** 解析请求 */
-    	( new Router() ).run(new RequestInfo(request), new ResponseInfo(response));
-    	
+			
+		/** 设置字符集编码 */
+		response.setContentType("text/html; charset=UTF-8");
+	
+		/** 解析请求 */
+		( new Router() ).run(new RequestInfo(request), new ResponseInfo(response));
+			
 	}
 }
